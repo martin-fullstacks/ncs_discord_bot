6dcc819ef8e956bed3aba92aa0bb68a485d7dc60
martin-fullstacks <mattia.vergnat@gmail.com>
2022-05-30 22:44:23 +0200

    :coffin: Remove useless imports.

--- slscommands/info/ban.js
+++ slscommands/info/ban.js
@@ -1,13 +1,3 @@
-const {
-    Client,
-    CommandInteraction,
-    MessageEmbed,
-    MessageActionRow,
-    MessageButton,
-    Message,
-} = require("discord.js");
-
-const wait = require("util").promisify(setTimeout);
 
 module.exports = {
     name: "ban",
