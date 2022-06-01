a2c141895760e5a3174d09ccb8e17f357af96ada
martin-fullstacks <mattia.vergnat@gmail.com>
2022-06-01 22:59:54 +0200

    :art: Change the structure of the code.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -47,4 +47,33 @@ client.on("interactionCreate", async (interaction) => {
         const command = client.slashCommands.get(interaction.commandName);
         if (command) command.run(client, interaction);
     }
+
+    if (!interaction.isSelectMenu()) return;
+    if (interaction.customId === "select") {
+        await interaction.deferUpdate();
+        const member = await interaction.guild.members.fetch(interaction.user.id);
+
+        let hasAlreadyRole = false;
+        for (const value in interaction.values) {
+            console.log(interaction.values[0]);
+            console.log("Value " + value);
+            const role = interaction.guild.roles.cache.get(interaction.values[0]);
+            console.log("Role " + role);
+            if (
+                interaction.member.roles.cache.some(
+                    (role) => role.id === interaction.values[0]
+                )
+            ) {
+                hasAlreadyRole = true;
+                await member.roles.remove(role);
+            } else {
+                await member.roles.add(role);
+            }
+        }
+        await interaction.followUp({
+            content: hasAlreadyRole
+                ? "You already have some of the roles, I've removed them. The others have been added."
+                : "Roles have been successfully added",
+            ephemeral: true,
+        });
 });
