ef5e94c4e4050464826b85d784516648c9fa91eb
foltone <77021937+foltone@users.noreply.github.com>
2022-06-02 23:14:29 +0200

    ✨ Add Leave & Join Member Events (#17)
    * :package: add cmd kick
    
    * :package: add mut and unmute
    
    * :hammer: add condition max timeout time
    
    * :wrench: remove the unused imports
    
    * :package: add un(lock) slashcomands
    
    * :hammer: add condition bot cannot lock for higher role
    
    * :hammer: delet useless code
    
    * :hammer: update condition for get highest role of bot
    
    * :hammer: update condition for get highest role of bot
    
    * :package: add event guildMember(Add&Remove)
    
    * :hammer: delet my channel id
    
    * :hammer: update condition for get highest role of bot
    
    * :hammer: remplace message embed to simple message
    
    * Fix merge conflicts
    
    Co-authored-by: Foltone <aug.viel@gmail.com>
    Co-authored-by: A1ex <a1ex@monika.gg>

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,41 @@
+const { MessageAttachment } = require('discord.js');
+const client = require("..");
+const config = require("../settings/config.json");
+const Canvas = require("canvas");
+
+client.on("guildMemberRemove", async member => {
+    if (!member.guild) return;
+
+    const {registerFont} = require('canvas')
+    const canvas = Canvas.createCanvas(1772, 633);
+    const context = canvas.getContext('2d');
+    const background = await Canvas.loadImage(`./img/welcome.png`);
+
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    context.strokeStyle = '#f2f2f2';
+    context.strokeRect(0, 0, canvas.width, canvas.height);
+
+    var textString3 = `${member.user.username}`;
+
+    if (textString3.length >= 14) {
+      context.font = 'bold 100px "Arial"';
+      context.fillStyle = '#f2f2f2';
+      context.fillText(textString3, 720, canvas.height / 2 + 20);
+    }
+    else {
+      context.font = 'bold 150px "Arial"';
+      context.fillStyle = '#f2f2f2';
+      context.fillText(textString3, 720, canvas.height / 2 + 20);
+    }
+    
+    context.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);
+    context.clip();
+
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+    context.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
+    const attachmentleav = new MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+     member.guild.channels.fetch(config.channelMessageleav)
+     .then((channel) => channel.send({ content:`Goodbye ${member.user}!`, files: [attachmentleav] }) )
+     .catch(console.error);
+});
