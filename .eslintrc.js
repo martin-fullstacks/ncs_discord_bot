7a1bce1c0a6ebbaa7a5982cdc449e52fb3dbab9f
A1ex <a1ex@monika.gg>
2022-06-02 23:15:10 +0200

    📦️💥 Add ESLint (#20)
    * 📦️ Bump Discord.JS to v13.7
    
    * 📦️⚠️ Add ESLint
    
    * 📦️⚠️ Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'env': {
+      'browser': true,
+      'commonjs': true,
+      'es2022': true,
+    },
+    'extends': [
+      'google',
+    ],
+    'parserOptions': {
+      'ecmaVersion': 13,
+    },
+    'rules': {
+      'linebreak-style': 0,
+      'object-curly-spacing': 0,
+      'require-jsdoc': 0,
+      'max-len': ['error', { 'code': 200 }],
+      'padded-blocks': 0,
+      'no-undef-init': 'error',
+    },
+  };
