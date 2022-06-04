f5379dd295a38d9c808cf033c5ab459698e6b091
martin-fullstacks <mattia.vergnat@gmail.com>
2022-06-04 13:31:58 +0200

    :bug: Fix conflicts.

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
-const client = require("..");
+const client = require('..');
 
-client.on("ready", () => {
+client.on('ready', () => {
     console.log(`\x1b[32m[BOT ON]\x1b[33m ${client.user.username} `);
 
-    client.user.setActivity(`at the devs.`, {
+    client.user.setActivity(config.botStatusMessage, {
         type: "WATCHING",
     });
 });
