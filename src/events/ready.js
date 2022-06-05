047c561ad7602262b90d31f3c9043c033d377f3b
martin-fullstacks <mattia.vergnat@gmail.com>
2022-06-05 20:40:54 +0200

    :hammer: Fix for EsLint.

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 const client = require('..');
-const config = require("../settings/config.json");
+const config = require('../settings/config.json');
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`\x1b[32m[BOT ON]\x1b[33m ${client.user.username} `);
 
   client.user.setActivity(config.botStatusMessage, {
